eea4a42d660112ed2f67a92a5455480f14c02fc2
Clement_Hue <clementhue@hotmail.fr>
2021-03-21 15:55:22 +0100

    maj task

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import sys
 
 DATA_FILE = sys.argv[1] if len(sys.argv) > 1 else "./data/tasks.json"
 if __name__ == "__main__":
-    loader = JsonLoader()
-    burden = loader.load_burden(DATA_FILE)
+    loader = JsonLoader(DATA_FILE)
+    burden = loader.load_burden()
     burden.show("ptrans")
